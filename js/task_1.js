d110b69fbeeb3166349631451f28cba1875635b6
Lakkusya <67253763+Lakkusya@users.noreply.github.com>
2020-09-20 22:24:37 +0300

    ДЗ 1 задачи 4 и 5

--- js/task_1.js
+++ js/task_1.js
@@ -10,5 +10,12 @@
 // 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'.
 const productName = "name";
 const productPrice = "price";
-
-console;
+name = "Генератор защитного поля";
+let price = 1000;
+const firstMessage =
+  "Выбран «Генератор защитного поля», цена за штуку 1000 кредитов";
+console.log(firstMessage);
+let price = 2000;
+const secondMessage =
+  "Выбран «Генератор защитного поля», цена за штуку 2000 кредитов";
+console.log(secondMessage);
